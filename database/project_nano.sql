2b25f127aaed77269b14184ff985fdfb388952ce
sonle <sonle@les-MacBook-Pro.local>
2021-03-07 16:29:52 +0700

    db

--- database/project_nano.sql
+++ database/project_nano.sql
@@ -1,67 +1,67 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 04, 2021 lúc 01:53 PM
--- Phiên bản máy phục vụ: 10.4.13-MariaDB
--- Phiên bản PHP: 7.4.8
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : mac local
+ Source Server Type    : MySQL
+ Source Server Version : 100508
+ Source Host           : localhost:3306
+ Source Schema         : project_nano
 
+ Target Server Type    : MySQL
+ Target Server Version : 100508
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 07/03/2021 16:27:36
+*/
 
---
--- Cơ sở dữ liệu: `project_nano`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `material` varchar(255) DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `visible` tinyint(255) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
---
--- Cấu trúc bảng cho bảng `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
-  `user_pass` varchar(50) NOT NULL,
-  `user_email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `users`
---
-
-INSERT INTO `users` (`id`, `user_name`, `user_pass`, `user_email`) VALUES
-(3, 'Trương Minh Lợi', '123456', 'loitruongminhit@gmail.com');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+BEGIN;
+INSERT INTO `product` VALUES (1, 'EIF gas', 'Đỏ', 'Thép', 15, 290000.00, 1);
+INSERT INTO `product` VALUES (2, 'LP Petrol', 'Đỏ', 'Composit', 13, 320000.00, 1);
+INSERT INTO `product` VALUES (3, 'Petrol Limex', 'Đỏ', 'Composit', 13, 320000.00, 1);
+INSERT INTO `product` VALUES (4, 'Nam Gas', 'Đỏ', 'Composit', 13, 320000.00, 1);
+INSERT INTO `product` VALUES (5, 'Thủ Đức Gas', 'Đỏ', 'Composit', 13, 320000.00, 1);
+INSERT INTO `product` VALUES (6, 'Nam Gas', 'Đỏ', 'Composit', 13, 320000.00, 1);
+INSERT INTO `product` VALUES (7, 'Thủ Đức Gas', 'Đỏ', 'Composit', 13, 320000.00, 1);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES (3, 'Trương Minh Lợi', '123456', 'loitruongminhit@gmail.com');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
